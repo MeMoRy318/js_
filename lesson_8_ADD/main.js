fe764d4b63f6fb9e6315e0b7c0787c41283c708d
User <girain3181@gmail.com>
2022-12-10 23:55:29 +0300

    finish

---
+++ lesson_8_ADD/main.js
@@ -0,0 +1,183 @@
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement. Всі данні в одному блоці.
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам (div>div*4)
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам , блок з адресою зробити окремим блоком, з блоками для кожної властивості
+// - З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
+
+const users = [{
+    name: 'vasya',
+    age: 31,
+    status: false,
+    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+}, {
+    name: 'petya',
+    age: 30,
+    status: true,
+    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+}, {
+    name: 'kolya',
+    age: 29,
+    status: true,
+    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+}, {
+    name: 'olya',
+    age: 28,
+    status: false,
+    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+}, {
+    name: 'max',
+    age: 30,
+    status: true,
+    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+}, {
+    name: 'anya',
+    age: 31,
+    status: false,
+    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+}, {
+    name: 'oleg',
+    age: 28,
+    status: false,
+    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+}, {
+    name: 'andrey',
+    age: 29,
+    status: true,
+    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+}, {
+    name: 'masha',
+    age: 30,
+    status: true,
+    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+}, {
+    name: 'olya',
+    age: 31,
+    status: false,
+    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+}, {
+    name: 'max',
+    age: 31,
+    status: true,
+    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+}];
+
+const users_ = document.createElement('div');
+users_.classList.add('users_');
+
+document.body.appendChild(users_)
+
+const usersAdres = [];
+
+for (const item of users) {
+
+    const users__card = document.createElement('div');
+    users__card.classList.add('users__card');
+
+    const users__title = document.createElement('h1');
+    users__title.classList.add('users__title');
+    users__title.innerText = item["name"];
+
+    const users__text = document.createElement('p');
+    users__text.classList.add('users__text');
+    users__text.innerText = `age - ${item["age"]} status - ${item["status"]}`;
+
+    const users__list = document.createElement('ul');
+    users__list.classList.add('users__list');
+
+    users_.appendChild(users__card);
+    users__card.append(users__title,users__text,users__list);
+
+    for (const key in item['address']) {
+
+        const users__item = document.createElement('li');
+        users__item.classList.add('users__item');
+        users__item.innerText = `${key} - ${item['address'][key]}`;
+
+        users__list.appendChild(users__item);
+    }
+    usersAdres.push(item["address"])
+}
+
+// - є сторінка rules.html. Контентом сторінки є заголовки та параграфи. Заголовки (h2) характеризують тему контенту яка вказана в параграфі.
+//     створити скріпт, котрий зчитує всі заголовки, та робить в блоці з id=content з них список(ul>li), який буде змістом того, що знаходиться на сторінці.
+//     Скріпт повинен працювати навіть якщо кількість блоків з заголовком та параграфом зміниться.
+
+const h2_element = document.getElementsByTagName('h2');
+const content = document.getElementById('content');
+
+const list_ = document.createElement('ul');
+list_.classList.add('list_');
+
+content.appendChild(list_)
+
+for (const item of h2_element) {
+
+    const list__item = document.createElement('li');
+    list__item.classList.add('list__item');
+    list__item.innerText = item.innerText;
+
+    list_.appendChild(list__item);
+}
+
+// -Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
+//     При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
+//     Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
+
+const rules_ = document.createElement('div');
+rules_.classList.add('rules');
+
+document.body.appendChild(rules_);
+
+let rules = [
+    {
+        title: 'Первое правило Бойцовского клуба.',
+        body: 'Никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Второе правило Бойцовского клуба.',
+        body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Третье правило Бойцовского клуба.',
+        body: 'В схватке участвуют только двое.'
+    },
+    {
+        title: 'Четвертое правило Бойцовского клуба.',
+        body: 'Не более одного поединка за один раз.'
+    },
+    {
+        title: 'Пятое правило Бойцовского клуба.',
+        body: 'Бойцы сражаются без обуви и голые по пояс.'
+    },
+    {
+        title: 'Шестое правило Бойцовского клуба.',
+        body: 'Поединок продолжается столько, сколько потребуется.'
+    },
+    {
+        title: 'Седьмое правило Бойцовского клуба.',
+        body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+    },
+    {
+        title: 'Восьмое и последнее правило Бойцовского клуба.',
+        body: 'Новичок обязан принять бой.'
+    },
+
+];
+
+for (const item of rules) {
+
+    const rules__card = document.createElement('div');
+    rules__card.classList.add('rules__card');
+
+    const rules__title = document.createElement('h2');
+    rules__title.classList.add('rules__title');
+    rules__title.innerText = item['title'];
+
+    const rules__text = document.createElement('p');
+    rules__text.classList.add('rules__title');
+    rules__text.innerText = item['body'];
+
+    rules__card.append(rules__title,rules__text);
+
+    rules_.appendChild(rules__card);
+}
+
